1df4b9ac9a12a0f1df7f19ad3f7d0bd240a886bf
victoria field <victoria.shadid@gmail.com>
2017-09-17 16:03:38 -0500

    added js code

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+
+var friends = [ {
+	"name" : "Elmo",
+	"photo" : "../images/elmo.jpg",
+	"scores" : [5,1,3,3,3,1,1,1,5,5]
+}, {
+	"name" : "Oscar",
+	"photo" : "../images/oscar.png",
+	"scores" : [1,5,1,5,1,5,5,5,1,1]
+}, {
+	"name" : "Ernie",
+	"photo" : "../images/Ernie-RubberDuckie.jpg",
+	"scores" : [3,3,5,1,5,3,3,3,3,3]
+}];
+
+
+module.exports = friends;
