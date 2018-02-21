ea25210a47a8cbe00c5349da820ac0f5c8476d70
victoria field <victoria.shadid@gmail.com>
2018-02-21 09:48:44 -0600

    corrected port for heroku push

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
-// ==============================================================================
-// ==============================================================================
-// DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
-// ==============================================================================
+
 
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path")
-// ==============================================================================
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
-// ==============================================================================
+
 
 // Tells node that we are creating an "express" server
 var app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // BodyParser makes it easy for our server to interpret data sent to it.
 // The code below is pretty standard.
@@ -27,19 +20,12 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use("/", express.static(path.resolve(__dirname, 'app/public')))
 
 
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
+// Routes
 
 require("./app/routing/api-routes")(app);
 require("./app/routing/html-routes")(app);
 
-// ==============================================================================
-// LISTENER
-// The below code effectively "starts" our server
-// ==============================================================================
+
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
